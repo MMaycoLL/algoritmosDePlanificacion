aae19a7be79513f95101fcb3117e9adcf8e93392
Maycol Agredo Castañeda <32054958+mmaykol@users.noreply.github.com>
2022-11-23 23:53:26 -0500

    Se agrega algoritmo HRN

--- main.py
+++ main.py
@@ -246,6 +246,132 @@ class SRTF(inputs):
         print(" Tiempo promedio de espera  : " + str(sum(self.tiempoEspera) / n))
 
 
+class HRN:
+    def processData(self, numeroProcesos):
+        process_data = []
+        for i in range(numeroProcesos):
+            temporary = []
+            process_id = int(input("Enter Process ID: "))
+
+            arrival_time = int(input(f"Enter Arrival Time for Process {process_id}: "))
+
+            burst_time = int(input(f"Enter Burst Time for Process {process_id}: "))
+
+            temporary.extend([process_id, arrival_time, burst_time, 0])
+            '''
+            '0' is the state of the process. 0 means not executed and 1 means execution complete
+            '''
+            process_data.append(temporary)
+        HRN.schedulingProcess(self, process_data)
+
+    def schedulingProcess(self, process_data):
+        start_time = []
+        exit_time = []
+        s_time = 0
+        sequence_of_processes = []
+        process_data.sort(key=lambda x: x[1])
+        '''
+        Sort processes according to the Arrival Time
+        '''
+        for i in range(len(process_data)):
+            ready_queue = []
+            temp = []
+            normal_queue = []
+            for j in range(len(process_data)):
+                if (process_data[j][1] <= s_time) and (process_data[j][3] == 0):
+                    response_ratio = 0
+                    response_ratio = float(((s_time - process_data[j][1]) + process_data[j][2]) / process_data[j][2])
+                    '''
+                    Calculating the Response Ratio foe each process
+                    '''
+                    temp.extend([process_data[j][0], process_data[j][1], process_data[j][2], response_ratio])
+                    ready_queue.append(temp)
+                    temp = []
+                elif process_data[j][3] == 0:
+                    temp.extend([process_data[j][0], process_data[j][1], process_data[j][2]])
+                    normal_queue.append(temp)
+                    temp = []
+            if len(ready_queue) != 0:
+                ready_queue.sort(key=lambda x: x[3], reverse=True)
+                '''
+                Sort the processes according to the Highest Response Ratio
+                '''
+                start_time.append(s_time)
+                s_time = s_time + ready_queue[0][2]
+                e_time = s_time
+                exit_time.append(e_time)
+                sequence_of_processes.append(ready_queue[0][0])
+                for k in range(len(process_data)):
+                    if process_data[k][0] == ready_queue[0][0]:
+                        break
+                process_data[k][3] = 1
+                process_data[k].append(e_time)
+            elif len(ready_queue) == 0:
+                if s_time < normal_queue[0][1]:
+                    s_time = normal_queue[0][1]
+                start_time.append(s_time)
+                s_time = s_time + normal_queue[0][2]
+                e_time = s_time
+                exit_time.append(e_time)
+                sequence_of_processes.append(normal_queue[0][0])
+                for k in range(len(process_data)):
+                    if process_data[k][0] == normal_queue[0][0]:
+                        break
+                process_data[k][3] = 1
+                process_data[k].append(e_time)
+        t_time = HRN.calculateTurnaroundTime(self, process_data)
+        w_time = HRN.calculateWaitingTime(self, process_data)
+        HRN.printData(self, process_data, t_time, w_time, sequence_of_processes)
+
+    def calculateTurnaroundTime(self, process_data):
+        total_turnaround_time = 0
+        for i in range(len(process_data)):
+            turnaround_time = process_data[i][4] - process_data[i][1]
+            '''
+            turnaround_time = completion_time - arrival_time
+            '''
+            total_turnaround_time = total_turnaround_time + turnaround_time
+            process_data[i].append(turnaround_time)
+        average_turnaround_time = total_turnaround_time / len(process_data)
+        '''
+        average_turnaround_time = total_turnaround_time / numeroProcesos
+        '''
+        return average_turnaround_time
+
+    def calculateWaitingTime(self, process_data):
+        total_waiting_time = 0
+        for i in range(len(process_data)):
+            waiting_time = process_data[i][5] - process_data[i][2]
+            '''
+            waiting_time = turnaround_time - burst_time
+            '''
+            total_waiting_time = total_waiting_time + waiting_time
+            process_data[i].append(waiting_time)
+        average_waiting_time = total_waiting_time / len(process_data)
+        '''
+        average_waiting_time = total_waiting_time / numeroProcesos
+        '''
+        return average_waiting_time
+
+    def printData(self, process_data, average_turnaround_time, average_waiting_time, sequence_of_processes):
+        process_data.sort(key=lambda x: x[0])
+        '''
+        Sort processes according to the Process ID
+        '''
+        print("Process_ID  Arrival_Time  Burst_Time      Completed  Completion_Time  Turnaround_Time  Waiting_Time")
+
+        for i in range(len(process_data)):
+            for j in range(len(process_data[i])):
+                print(process_data[i][j], end="\t\t\t\t")
+            print()
+
+        print(f'Average Turnaround Time: {average_turnaround_time}')
+
+        print(f'Average Waiting Time: {average_waiting_time}')
+
+        print(f'Sequence of Processes: {sequence_of_processes}')
+
+
 # driver's code
 
 
@@ -263,6 +389,7 @@ while (1):
     print(" **   4.         Prioridad no expulsiva **")
     print(" **   5.                    SJF         **")
     print(" **   6.                   SRTF         **")
+    print(" **   7.                    HRN         **")
     print(" **   0.                   Salir        **")
     print(" *****************************************")
 
@@ -327,3 +454,8 @@ while (1):
         srtf.getTiempoRespuesta()
         srtf.getTiempoEspera()
         srtf.printSrtf()
+
+    if option == 7:
+        numeroProcesos = n
+        hrn = HRN()
+        hrn.processData(numeroProcesos)
